fe1ebe7c9f527fe1cfcad2f8ac769d3ee8e41e91
Ivan <ashikhin1998@gmail.com>
2023-02-21 00:42:46 +0600

    Добавление продукта
    Реализовал добавление продукта

--- app/webapp/urls.py
+++ app/webapp/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-
 from webapp.views.base import index_view
-
-from webapp.views.products import category_add_view, detail_view
+from webapp.views.products import category_add_view, detail_view, product_add_view
 
 urlpatterns = [
     path("", index_view, name="index_page"),
-    path('product/', detail_view),
+    path('product/<int:pk>', detail_view, name ='details'),
     path('categories/add', category_add_view, name='category_add'),
+    path('product/add', product_add_view, name='product_add'),
 ]
--- app/webapp/views/products.py
+++ app/webapp/views/products.py
@@ -1,13 +1,12 @@
 from django.http import request
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from webapp.models import Product
 from webapp.models import Category
 
 
-def detail_view(request):
-    product_pk = request.GET.get('pk')
-    product = Product.objects.get(pk=product_pk)
+def detail_view(request, pk):
+    product = get_object_or_404(Product,pk=pk)
     context = {'product': product}
     return render(request, 'detail.html', context=context)
 
@@ -15,9 +14,23 @@ def detail_view(request):
 def category_add_view(request):
     if request.method == "GET":
         return render(request, 'add_category.html')
-    article_data = {
+    category_data = {
         'category_name': request.POST.get('category_name'),
         'category_description': request.POST.get('category_description'),
     }
-    category = Category.objects.create(**article_data)
+    Category.objects.create(**category_data)
+    return redirect('index_page')
+
+def product_add_view(request):
+    if request.method == "GET":
+        categories = Category.objects.all()
+        return render(request, 'add_product.html', context={"categories": categories})
+    product_data = {
+        'product_name': request.POST.get('product_name'),
+        'cost': request.POST.get('cost'),
+        'image': request.POST.get('image'),
+        'category_id': int(request.POST.get('category_id'))
+    }
+    Product.objects.create(**product_data)
+    return redirect('index_page')
 
